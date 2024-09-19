989f16d39ffaf9bcf3626ff5506c17bc4ba50291
DespairP <328060206@qq.com>
2024-09-19 21:13:16 +0800

    ✨ Initialize Project

---
+++ src/main/java/org/teamHTBP/extraascension/EasyOfAscensionExtra.java
@@ -0,0 +1,9 @@
+package org.teamHTBP.extraascension;
+
+import net.neoforged.fml.common.Mod;
+
+@Mod(EasyOfAscensionExtra.MOD_ID)
+public class EasyOfAscensionExtra
+{
+    public static final String MOD_ID = "eoa_ex";
+}
